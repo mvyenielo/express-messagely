307c30238f02f317f14a8ec7f36c93e8c31e12a7
Megan Vyenielo <mvyenielo58@gmail.com>
2023-09-18 12:00:32 -0700

    completed authenticate, register, and updateLoginTimestamp methods for User class

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 "use strict";
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
 
 /** User of the site. */
 
@@ -9,16 +11,51 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]
+    );
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+
+    const result = await db.query(
+      `SELECT password
+        FROM users
+        WHERE username = $1`,
+      [username]
+    );
+
+    const user = result.rows[0];
+
+    if (user) {
+      return await bcrypt.compare(password, user.password);
+    }
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+
+    const result = await db.query(
+      `UPDATE users
+        SET last_login_at = SYSDATETIMEOFFSET()
+        WHERE username = $1
+        RETURN last_login_at`,
+      [username]
+    );
+
+    const lastLogin = result.rows[0];
+
+    return lastLogin;
   }
 
   /** All: basic info on all users:
