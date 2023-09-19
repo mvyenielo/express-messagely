7a407658b74be85afbb77c6b5b6344d5aee37333
Nathan Tjahjadi <nathantja@yahoo.com>
2023-09-19 13:29:37 -0500

    users routes test completed

--- routes/users.test.js
+++ routes/users.test.js
@@ -6,6 +6,7 @@ const jwt = require("jsonwebtoken");
 const app = require("../app");
 const db = require("../db");
 const User = require("../models/user");
+const Message = require("../models/message");
 
 const { SECRET_KEY } = require("../config");
 
@@ -25,6 +26,20 @@ describe("All User Routes", function () {
       first_name: "Test1",
       last_name: "Testy1",
       phone: "+14155550000",
+    });
+
+    let u2 = await User.register({
+      username: "test2",
+      password: "password",
+      first_name: "Test2",
+      last_name: "Testy2",
+      phone: "+15155550000",
+    });
+
+    await Message.create({
+      from_username: "test1",
+      to_username: "test2",
+      body: "Test Message!"
     });
 
     const u1Payload = { username: "test1" };
@@ -53,6 +68,11 @@ describe("All User Routes", function () {
               username: "test1",
               first_name: "Test1",
               last_name: "Testy1",
+            },
+            {
+              username: "test2",
+              first_name: "Test2",
+              last_name: "Testy2",
             }
           ]
         }
@@ -179,8 +199,128 @@ describe("All User Routes", function () {
         }
       );
     });
+  });
 
+  describe("GET /users/:username/to", function () {
+    test("GET /users/test2/to", async function () {
+      const params = new URLSearchParams({ _token: u2Token });
 
+      let response = await request(app)
+        .get(`/users/test2/to?${params}`);
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(
+        {
+          "messages": [
+            {
+              "id": expect.any(Number),
+              "from_user": {
+                "username": "test1",
+                "first_name": "Test1",
+                "last_name": "Testy1",
+                "phone": "+14155550000"
+              },
+              "body": "Test Message!",
+              "sent_at": expect.anything(),
+              "read_at": null
+            }
+          ]
+        }
+      );
+    });
+
+    test("GET /users/test2/to when no token", async function () {
+      let response = await request(app)
+        .get(`/users/test2/to`);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual(
+        {
+          "error": {
+            "message": "Unauthorized",
+            "status": 401
+          }
+        }
+      );
+    });
+
+    test("GET /users/test2/to with mismatched user/token", async function () {
+      const params = new URLSearchParams({ _token: u1Token });
+
+      let response = await request(app)
+        .get(`/users/test2/to?${params}`);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual(
+        {
+          "error": {
+            "message": "Unauthorized",
+            "status": 401
+          }
+        }
+      );
+    });
+  });
+
+  describe("GET /users/:username/from", function () {
+    test("GET /users/test1/from", async function () {
+      const params = new URLSearchParams({ _token: u1Token });
+
+      let response = await request(app)
+        .get(`/users/test1/from?${params}`);
+
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(
+        {
+          "messages": [
+            {
+              "id": expect.any(Number),
+              "to_user": {
+                "username": "test2",
+                "first_name": "Test2",
+                "last_name": "Testy2",
+                "phone": "+15155550000"
+              },
+              "body": "Test Message!",
+              "sent_at": expect.anything(),
+              "read_at": null
+            }
+          ]
+        }
+      );
+    });
+
+    test("GET /users/test1/from when no token", async function () {
+      let response = await request(app)
+        .get(`/users/test1/from`);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual(
+        {
+          "error": {
+            "message": "Unauthorized",
+            "status": 401
+          }
+        }
+      );
+    });
+
+    test("GET /users/test1/from with mismatched user/token", async function () {
+      const params = new URLSearchParams({ _token: u2Token });
+
+      let response = await request(app)
+        .get(`/users/test1/from?${params}`);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual(
+        {
+          "error": {
+            "message": "Unauthorized",
+            "status": 401
+          }
+        }
+      );
+    });
   });
 });
 
